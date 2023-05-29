40f1bd168760dc9b5fe8385991f0fcdffd9afe60
Anirudh <an516658@dal.ca>
2023-05-29 10:08:39 -0300

    folder structure

--- quiz-app-frontend/src/components/LandingPage.js
+++ quiz-app-frontend/src/components/LandingPage.js
@@ -1,51 +1,36 @@
 import React from 'react';
 import Button from '@mui/material/Button';
 import Box from '@mui/material/Box';
-import { makeStyles } from '@mui/styles';
 
-const useStyles = makeStyles(() => ({
-  root: {
+const LandingPage = () => {
+  const rootStyles = {
     display: 'flex',
     justifyContent: 'center',
     alignItems: 'center',
     minHeight: '100vh',
     background: 'linear-gradient(to right, #004d7a, #00c6fb)',
     fontFamily: 'Roboto, sans-serif',
-  },
-  container: {
+  };
+
+  const containerStyles = {
     textAlign: 'center',
-  },
-  button: {
-    padding: '15px 30px',
-    fontSize: '24px',
-    fontWeight: 'bold',
-    borderRadius: '5px',
-    background: 'linear-gradient(to right, #ff0084, #ff66cc)',
-    color: '#fff',
-    cursor: 'pointer',
-    textDecoration: 'none',
-    '&:hover': {
-      background: 'linear-gradient(to right, #c70055, #ff0066)',
-    },
-  },
-  text: {
+  };
+
+  const textStyles = {
     fontSize: '32px',
     fontWeight: 'bold',
     color: '#fff',
     marginBottom: '20px',
-  },
-}));
+  };
 
-const LandingPage = () => {
-  const classes = useStyles();
 
   return (
-    <Box className={classes.root}>
-      <Box className={classes.container}>
-        <div className={classes.text}>
+    <Box sx={rootStyles}>
+      <Box sx={containerStyles}>
+        <Box component="div" sx={textStyles}>
           Quiz? Quizzer? Quizzest
-        </div>
-        <Button className={classes.button}>Let's Quiz</Button>
+        </Box>
+        <Button color="success" variant='contained'>Let's Quiz</Button>
       </Box>
     </Box>
   );
