155b626ad4337f0da4bfb1b8ac1aa8caa0cdcd0f
Shubham Mishra <sh277446@dal.ca>
2023-06-12 19:45:09 -0300

    Fix routes to open landing page

--- frontend/src/services/Routes.js
+++ frontend/src/services/Routes.js
@@ -4,15 +4,17 @@ import WelcomePage from '../components/team-management/WelcomePage';
 import TeamOperations from '../components/team-management/TeamOperations';
 import SignIn from '../components/user-authentication/SignIn';
 import SecurityForm from '../components/user-authentication/SecurityForm';
+import Profile from '../components/profile-management/Profile';
 
 const Routes = () => {
     return (
         <Rt>
-            <Route path="/" element={<SignIn />} />
+            <Route path="/" element={<LandingPage />} />
             <Route path="/SignIn" element={<SignIn />} />
             <Route path="/welcome" element={<WelcomePage />} />
             <Route path="/TeamOperations" element={<TeamOperations />} />
             <Route path="/SecurityForm" element={<SecurityForm/>} />
+            <Route path="/ProfilePage" element={<Profile/>} />
         </Rt>
     )
 }
