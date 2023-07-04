2bee6b6fa2a46b3a13b673180c0ee84ed533511a
Shubham Mishra <sh277446@dal.ca>
2023-07-04 18:33:13 -0300

    Fetch profile data of a user

--- frontend/src/components/profile-management/Profile.js
+++ frontend/src/components/profile-management/Profile.js
@@ -1,8 +1,10 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import { Grid, Typography, Avatar, TextField, Button, Input } from "@mui/material";
 import { useNavigate, Navigate } from "react-router-dom";
 import { AuthContext } from "../../services/AuthContext";
 import AWS from "aws-sdk";
+import { convertLength } from "@mui/material/styles/cssUtils";
+const logo = require('../../images/profile.png');
 
 
 const AWS_CONFIG = {
@@ -16,14 +18,60 @@ AWS.config.update(AWS_CONFIG);
 
 const lambda = new AWS.Lambda({ region: process.env.REACT_APP_AWS_REGION });
 
-
 const Profile = () => {
   const navigate = useNavigate();
   const [name, setName] = useState("");
   const [contactNumber, setContactNumber] = useState("");
   const [profilePicture, setProfilePicture] = useState(null);
+  const [profilePictureFile, setProfilePictureFile] = useState(null);
   const { currentUser } = useContext(AuthContext);
   const [profileUpdateResponse, setprofileUpdateResponse] = useState("");
+  const [isLoading, setIsLoading] = useState(true);
+
+
+  const fetchProfile = async () => {
+    try {
+      const params = {
+        FunctionName: "profile",
+        Payload: JSON.stringify({
+          functionName: "getProfile",
+          userId: currentUser.uid,
+        }),
+      };
+
+      const data = await lambda.invoke(params).promise();
+      console.log('-------lambda-----------');
+
+      if (data.StatusCode === 200) {
+        const profileData = JSON.parse(JSON.parse(data.Payload).body);
+
+        setName(profileData.name);
+        setContactNumber(profileData.contactNumber);
+        // if (profileData.image === '') {
+          console.log('defaultimage-----',profileData.image);
+          setProfilePicture(profileData.image);
+        // } else {
+        //   setProfilePicture(profileData.image);
+        // }
+        console.log('-------lambda-200----------');
+      } else {
+        console.error("Error fetching profile data");
+      }
+
+      setIsLoading(false);
+    } catch (error) {
+      console.error("Error:", error);
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+
+    if (currentUser) {
+      console.log('-------hi-----------');
+      fetchProfile();
+    }
+  }, [currentUser]);
 
   const handleNameChange = (e) => {
     setName(e.target.value);
@@ -35,36 +83,58 @@ const Profile = () => {
 
   const handleProfilePictureChange = (e) => {
     const file = e.target.files[0];
-    setProfilePicture(file);
+    setProfilePictureFile(file);
   };
 
-  const handleSaveChanges = () => {
+  const handleSaveChanges = async () => {
     const reader = new FileReader();
 
     reader.onload = async (event) => {
       try {
         const imageBase64 = event.target.result.split(",")[1];
         const params = {
-          FunctionName: "updateProfile",
-          Payload: JSON.stringify({ userId: currentUser.uid, name, contactNumber, image: imageBase64}),
+          FunctionName: "profile",
+          Payload: JSON.stringify({ functionName: 'updateProfile',  userId: currentUser.uid, name, contactNumber, image: imageBase64}),
         };
 
         const data = await lambda.invoke(params).promise();
-
         if (data.StatusCode === 200) {
+          console.log("data.StatusCode === 200");
           setprofileUpdateResponse('Profile successfully updated');
-        } else {
+        } else {  
+          console.log('Error in updating profile');
           setprofileUpdateResponse('Error in updating profile');
         }
       } catch (error) {
         console.error("Error:", error);
         setprofileUpdateResponse('Error in updating profile');
       }
+      fetchProfile();
+
     };
 
-    if (profilePicture) {
-      reader.readAsDataURL(profilePicture);
+    if (profilePictureFile) {
+      reader.readAsDataURL(profilePictureFile);
     } else {
+      try {
+        const params = {
+          FunctionName: "profile",
+          Payload: JSON.stringify({ functionName: 'updateProfile',  userId: currentUser.uid, name, contactNumber}),
+        };
+
+        const data = await lambda.invoke(params).promise();
+        if (data.StatusCode === 200) {
+          console.log("data.StatusCode === 200");
+          setprofileUpdateResponse('Profile successfully updated');
+        } else {  
+          console.log('Error in updating profile');
+          setprofileUpdateResponse('Error in updating profile');
+        }
+      } catch (error) {
+        console.error("Error:", error);
+        setprofileUpdateResponse('Error in updating profile');
+      }
+      fetchProfile();
 
     }
   };
@@ -81,8 +151,8 @@ const Profile = () => {
       </Grid>
       <Grid item xs={12}> 
         {profilePicture && <img
-          src={URL.createObjectURL(profilePicture)}
-          alt="Profile Picture"
+          src={`${profilePicture}?${Date.now()}`}
+          alt="Profile"
           style={{ maxHeight: "100px", width: "auto" }}
         />}
       </Grid>
