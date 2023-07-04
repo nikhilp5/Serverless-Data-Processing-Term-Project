f704338858df3971ca11a1eed512771bd517fea2
Shubham Mishra <sh277446@dal.ca>
2023-07-04 18:32:26 -0300

    lambda function to fetch profile

--- backend/profile-management/index.js
+++ backend/profile-management/index.js
@@ -3,6 +3,16 @@ const s3 = new AWS.S3();
 const dynamodb = new AWS.DynamoDB();
 
 exports.handler = async (event) => {
+  const functionName = event.functionName;
+  if (functionName === 'updateProfile') {
+    return updateProfile(event)
+  } else if (functionName === 'getProfile') {
+    return getProfile(event)
+  }
+
+};
+
+async function updateProfile(event) {
   const { userId, image, name, contactNumber } = event;
 
   const getItemParams = {
@@ -53,7 +63,7 @@ exports.handler = async (event) => {
     const uploadParams = {
       Bucket: 'serverless-profile-images',
       Key: `${userId}.jpg`,
-      Body: image,
+      Body: Buffer.from(image, 'base64'),
     };
 
     await s3.upload(uploadParams).promise();
@@ -69,3 +79,68 @@ exports.handler = async (event) => {
     };
   }
 };
+
+async function getProfile(event) {
+  const { userId } = event;
+
+  const getItemParams = {
+    TableName: 'profile_info',
+    Key: {
+      userId: { S: userId },
+    },
+  };
+
+  try {
+    const userProfile = await dynamodb.getItem(getItemParams).promise();
+
+    if (userProfile.Item) {
+      // User profile found
+      const { name, contactNumber } = userProfile.Item;
+
+      // Get the image from S3
+      const imageKey = `${userId}.jpg`;
+      const getObjectParams = {
+        Bucket: 'serverless-profile-images',
+        Key: imageKey,
+      };
+
+      try {
+        const s3Object = await s3.getObject(getObjectParams).promise();
+        const image = s3Object.Body.toString('base64');
+
+        return {
+          statusCode: 200,
+          body: JSON.stringify({
+            name: name.S,
+            contactNumber: contactNumber.S,
+            image: `https://serverless-profile-images.s3.amazonaws.com/${userId}.jpg`,
+          }),
+        };
+      } catch (error) {
+        // Image not found in S3
+        const defaultImage = 'https://serverless-profile-images.s3.amazonaws.com/profile.png';
+        
+        return {
+          statusCode: 200,
+          body: JSON.stringify({
+            name: name.S,
+            contactNumber: contactNumber.S,
+            image: defaultImage,
+          }),
+        };
+      }
+    } else {
+      // User profile not found
+      return {
+        statusCode: 404,
+        body: 'User profile not found',
+      };
+    }
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: 'Error retrieving user profile',
+    };
+  }
+}
+
