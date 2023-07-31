2669f951226ad568f61ee2a2cf52398d141bb8c3
Anirudh <an516658@dal.ca>
2023-07-31 19:51:24 -0300

    Added FB for auth

--- frontend/src/components/user-authentication/SignIn.js
+++ frontend/src/components/user-authentication/SignIn.js
@@ -20,6 +20,7 @@ const uiConfig = {
   signInOptions: [
     firebase.auth.EmailAuthProvider.PROVIDER_ID,
     firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+    firebase.auth.FacebookAuthProvider.PROVIDER_ID
   ],
   callbacks: {
     signInSuccessWithAuthResult: () => false,
