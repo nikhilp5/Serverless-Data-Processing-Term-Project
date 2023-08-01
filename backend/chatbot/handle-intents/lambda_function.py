c8a2b4298cb1b7506336af4401e0d314f5dff57d
Shubham Mishra <sh277446@dal.ca>
2023-08-01 17:02:36 -0300

    Display team stats for a given team name in chatbot

--- backend/chatbot/handle-intents/lambda_function.py
+++ backend/chatbot/handle-intents/lambda_function.py
@@ -74,14 +74,33 @@ def handleNavIntent(event):
 def handleTeamScoresIntent(event):
     slots = event['sessionState']['intent']['slots']
     team_name = slots['teamName']['value']['interpretedValue'].lower()
-    response_str = f'{team_name} score is 100'
+    
+    api_url = f'https://jmflaholi8.execute-api.us-east-1.amazonaws.com/dev/getteamstatsbyname?teamName={team_name}'
+    
+    try:
+        # Make a GET request to the API
+        response = requests.get(api_url)
+        if response.status_code == 200:
+            data = response.json()
+            team_scores = data[0]
+            total_matches = team_scores.get('totalMatches', 0)
+            total_wins = team_scores.get('totalWins', 0)
+            total_losses = team_scores.get('totalLosses', 0)
+            total_points = team_scores.get('totalPoints', 0)
+            response_str = f"{team_name} stats:\nTotal Matches: {total_matches}\nTotal Wins: {total_wins}\nTotal Losses: {total_losses}\nTotal Points: {total_points}"
+        else:
+            response_str = f'Team stats not found for team: {team_name}'
+    except requests.RequestException as e:
+        print('Error fetching team score:', e)
+        response_str = f'Error fetching team stats for {team_name}'
+    
     response = {
         "sessionState": {
             "dialogAction": {
                 "type": "Close"
             },
             "intent": {
-                "name": "NavigationIntent",
+                "name": "TeamScoresIntent",
                 "state": "Fulfilled"
             }
         },
